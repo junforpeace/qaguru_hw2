790889f29c2c1e6b1cfa5ad344dd5e1eddd967de
junforpeace <soapcream@gmail.com>
2021-12-14 22:40:30 +0100

    начало теста

---
+++ src/test/java/com/oksarkh/FirstTest.java
@@ -0,0 +1,19 @@
+package com.oksarkh;
+
+import org.junit.jupiter.api.*;
+
+public class FirstTest {
+
+    @BeforeAll
+
+    @BeforeEach
+
+    @AfterAll
+
+    @AfterEach
+
+    @Test
+    void FirstTest() {
+        Assertions.assertTrue(true);
+    }
+}
